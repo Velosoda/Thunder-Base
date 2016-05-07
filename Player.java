038f355530f0ace573b2c17a0ca1cdc63db8561a
Velosoda <velosoda@kean.edu>
2016-05-07 16:33:49 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,6 +1,15 @@
-
-public class Player {
-	int roundsWon=0;
-	int gamesWon=0;
+package TBA;
+import java.util.ArrayList;
+public class Player{
 	
+	int squadMembers=5;
+	ArrayList<CounterTerrorists>squad=new ArrayList<CounterTerrorists>(squadMembers);
+	Player(){}
+	//refills/fills the squad array 
+	public void fillSquad(){
+		if(squad.size()!=squadMembers){
+			squad.add(new CounterTerrorists());
+			fillSquad();
+		}
+	}
 }
