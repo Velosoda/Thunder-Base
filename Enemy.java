0971117f4bea71d2a0fc58e901332dda529d7476
Velosoda <velosoda@kean.edu>
2016-05-07 16:03:18 -0400

    Update Enemy.java

--- Enemy.java
+++ Enemy.java
@@ -1,56 +1,115 @@
+package TBA;
+import java.util.ArrayList;
+import java.util.Random;
+public class Enemy{
+	Random r = new Random();
 
-public class Enemy extends Troop{
-	int charges=0;
-	int flashes=0;
-	int windowLeft=0;
-	int windowRight=0;
-	int door=0;
-	int ceiling=0;
-	int equipment;
-	//approaches
-	//will return 1 for door flash or 2 for doorCharge
-	public int door(Room room, Troop troopleft, Troop troopright, int charge, int flash){
-		door=1;
-		int doorFlash=0;
-		int doorCharge=0;
-		if(flashes==2){
-			System.out.println("You have no more flashes");
-			if(flash==1){
-				flashes=1;
-				charges=0;
-				equipment+=1;
-				if(room.doorLeft+room.doorRight>=1){
-					troopleft.health=0;
-					troopright.health=0;
+	//The numbers with in these boxes are used to give the Data some form of id 
+	//for later use in the program
+	//*************************************************************************
+	//these two will tell us how many of each type of equipment the enemy has *
+	int flashes=0;		//1													  *
+	int charges=0;		//2							  						  *
+	//*************************************************************************
+	
+	//these are used to properly randomize setup procedures
+	int initApproachRan=4;
+	int initEquipRan=2;
+	
+	//**************************************************
+	//these four define what approach the AI will take *
+	int windowLeft=0;   		//1					   *
+	int windowRight=0;  		//2					   *
+	int doorApproach=0;   		//3					   *
+	int ceilingApproach=0;		//4					   *
+	//**************************************************
+	
+	int squadMembers=5;
+	
+	//******************************************************************************
+	//These ArrayLists are currently being used to house the enemy troops as well  *
+	//as to give the programmer an idea as to which approach the enemy is going to *
+	//take																		   *
+	ArrayList<Terrorists>leftWindow=new ArrayList<Terrorists>();				 //*
+	ArrayList<Terrorists>rightWindow=new ArrayList<Terrorists>();				 //*
+	ArrayList<Terrorists>door=new ArrayList<Terrorists>();						 //*
+	ArrayList<Terrorists>ceiling=new ArrayList<Terrorists>();					 //*
+	//******************************************************************************
+	
+	//Ai positions will randomly assign a position for the enemy to take 
+	public void aiPositions(){
+		int approachRan = r.nextInt(initApproachRan)+1;
+		
+		if(squadMembers>0){
+			if(approachRan==1){
+				windowLeft=1;
+				leftWindow.add(new Terrorists());
+				leftWindow.get(0).approach=1;
+				squadMembers--;
+				initApproachRan=3;
+				aiPositions();
+			}
+			if(approachRan==2){
+				windowRight=1;
+				rightWindow.add(new Terrorists());
+				rightWindow.get(0).approach=2;
+				squadMembers--;		
+				initApproachRan=3;
+				aiPositions();
+			}
+			if(approachRan==3){
+				doorApproach=1;
+				door.add(new Terrorists());
+				door.get(0).approach=3;
+				squadMembers--;
+				initApproachRan=3;
+				aiPositions();
+			}
+			
+			if(approachRan==4){
+				ceilingApproach=1;
+				for(int i=0; i<squadMembers; i++){
+					ceiling.add(new Terrorists());
 				}
-				doorFlash=1;
-				return doorFlash;
+				ceiling.get(0).approach=4;
+				squadMembers-=5;
+				return;
 			}
 		}
-		if(charge==1){
+		else return;
+	}
+	
+	//will give the enemies a certain amount of equipment that they can use
+	//outcomes are 2flash 0charge or 1flash 1charge
+	public void equipmentGet(){
+		int equipRan = r.nextInt(initEquipRan)+1;
+		if(ceiling.size()==5){
 			charges=1;
-			doorCharge=2;
-			return doorCharge;
+			return;
 		}
-		else return 0;
-
+		if(flashes+charges<2){
+			if(equipRan==1){
+				flashes++;
+				equipmentGet();
+			}
+			if(equipRan==2){
+				charges=1;
+				equipmentGet();
+			}
+		}
 	}
-	public void windowLeft(int charge, int flash){
-		
-	}
-	public void windowRight(int charge, int flash){
-		
-	}
-	public void ceiling(){
-		
-	}
-	public void regular(){
-		
-	}
-	public void equipmentRunout(){
-		if(equipment==2)System.out.println("you do not have enough equipment for that approach so this will be taken regularly");
-	}
-	public void chargeEffect(){
-		
+	public void equipmentEquip(int equipmentInventory, int equipmentId, ArrayList<Terrorists> array){
+		if(ceiling.size()==5){
+			ceiling.get(0).equipment=1;
+			charges=0;
+			flashes=0;
+			return;
+		}
+		if(equipmentInventory>0){
+			if(array.size()>0 && array.get(0).equipment==0){
+				array.get(0).equipment=equipmentId;
+			}
+		}
+		else return;
 	}
 }
